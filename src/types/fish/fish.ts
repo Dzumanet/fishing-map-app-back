3445ca4606cdde8e3f4c8c8529b0ddc6d2cf6fa0
Dzumanet <137023861+Dzumanet@users.noreply.github.com>
2023-11-05 15:13:58 +0100

    Created interfaces.

---
+++ src/types/fish/fish.ts
@@ -0,0 +1,22 @@
+export interface FishInterface {
+  id: string;
+  fishName: string;
+  weight: number;
+  description: string;
+  catchDateTime: Date;
+  lat: number;
+  lon: number;
+}
+
+export type CreateFishResponse = {
+  success: boolean;
+  fish: FishInterface;
+};
+
+export type OneFishWithUser = {
+  fish: FishInterface;
+  userName: string;
+  id: string;
+};
+
+export type GetListOfFishResponse = FishInterface[];
---
+++ src/types/user/user.ts
@@ -0,0 +1,22 @@
+import { FishInterface } from '../fish';
+
+export interface UserInterface {
+  id: string;
+  userName: string;
+  createdAt: Date;
+  mainFishingSpotLat: number;
+  mainFishingSpotLon: number;
+}
+export interface RegisterUserResponse {
+  id: string;
+  userName: string;
+}
+
+export class RegisterUserDto {
+  userName: string;
+  pwd: string;
+}
+
+export interface FishWithUserInterface extends FishInterface {
+  user: UserInterface;
+}
